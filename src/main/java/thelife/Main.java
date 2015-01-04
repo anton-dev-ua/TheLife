48a2baf1c9e1933af20b295deaa0f4aaf2215898
Anton <box.of.anton@gmail.com>
2015-01-04 09:28:05 +0100

    tuning scene visualisation

--- src/main/java/thelife/Main.java
+++ src/main/java/thelife/Main.java
@@ -27,19 +27,19 @@ public class Main extends Application {
     private static final Color gridColor = Color.GRAY;
     private static final Color background = Color.WHITE;
     private static final Color cellColor = Color.BLACK;
-    private int sceneWidth = 900;
-    private int sceneHeight = 700;
-    private int sceneCellSize = 9;
+    private double sceneWidth = 900;
+    private double sceneHeight = 600;
+    private double sceneCellSize = 9;
 
-    private int sceneCenterX = 0;
-    private int sceneCenterY = 0;
+    private double sceneCenterX = 0;
+    private double sceneCenterY = 0;
 
-    private int sceneColumns = sceneWidth / sceneCellSize;
-    private int sceneRows = sceneHeight / sceneCellSize;
-    private int sceneBottom = sceneCenterY - sceneRows / 2 + 1;
-    private int sceneLeft = sceneCenterX - sceneColumns / 2;
-    private int sceneTop = sceneCenterY + sceneRows / 2;
-    private int sceneRight = sceneCenterX + sceneColumns / 2 - 1;
+    private double sceneColumns = sceneWidth / sceneCellSize;
+    private double sceneRows = sceneHeight / sceneCellSize;
+    private double sceneBottom = sceneCenterY - sceneRows / 2;
+    private double sceneLeft = sceneCenterX - sceneColumns / 2;
+    private double sceneTop = sceneCenterY + sceneRows / 2;
+    private double sceneRight = sceneCenterX + sceneColumns / 2 - 1;
 
 
     private Group sceneCells;
@@ -47,16 +47,21 @@ public class Main extends Application {
     private Space space;
     private Group lines;
     private boolean simulate;
+    private double fieldOffsetY;
+    private double fieldOffsetX;
 
-    private void changeScale(int newCellSize) {
+    private void changeScale(double newCellSize) {
         sceneCellSize = newCellSize;
 
         sceneColumns = sceneWidth / sceneCellSize;
         sceneRows = sceneHeight / sceneCellSize;
-        sceneBottom = sceneCenterY - sceneRows / 2 + 1;
+        sceneBottom = sceneCenterY - sceneRows / 2;
         sceneLeft = sceneCenterX - sceneColumns / 2;
         sceneTop = sceneCenterY + sceneRows / 2;
-        sceneRight = sceneCenterX + sceneColumns / 2 - 1;
+        sceneRight = sceneCenterX + sceneColumns / 2;
+
+        fieldOffsetY = sceneBottom * sceneCellSize + sceneHeight;
+        fieldOffsetX = -sceneLeft * sceneCellSize;
 
         drawGrid();
         displayLife();
@@ -93,7 +98,7 @@ public class Main extends Application {
         grid.add(new TextField("0"), 1, 2);
 
         initWorld();
-
+        changeScale(35);
         displayLife();
 
         primaryStage.setScene(scene);
@@ -108,6 +113,7 @@ public class Main extends Application {
         GridPane topGroup = new GridPane();
         topGroup.setHgap(10);
 
+        addButton(topGroup, "Reset", event -> reset());
         addButton(topGroup, "Run", event -> run());
         addButton(topGroup, "Stop", event -> simulate = false);
         addButton(topGroup, "+", event -> changeScale(sceneCellSize + 1));
@@ -165,21 +171,34 @@ public class Main extends Application {
         world = new World(space);
     }
 
-    private Group drawGrid() {
+    private void reset() {
+        initWorld();
+        displayLife();
+    }
+
+    private void drawGrid() {
         lines.getChildren().clear();
-        for (int i = 0; i <= sceneRows; i++) {
-            Line line = new Line(0, i * sceneCellSize, sceneColumns * sceneCellSize, i * sceneCellSize);
-            line.setStroke(gridColor);
-            line.setStrokeWidth(0.5);
-            lines.getChildren().add(line);
+
+        for (int y = (int) sceneBottom; y <= sceneTop; y++) {
+            addGridLine(0, fieldOffsetY - y * sceneCellSize, sceneColumns * sceneCellSize, fieldOffsetY - y * sceneCellSize);
         }
-        for (int i = 0; i <= sceneColumns; i++) {
-            Line line = new Line(i * sceneCellSize, 0, i * sceneCellSize, sceneRows * sceneCellSize);
-            line.setStroke(gridColor);
-            line.setStrokeWidth(0.5);
-            lines.getChildren().add(line);
+
+        for (int x = (int) sceneLeft; x <= sceneRight; x++) {
+            addGridLine(fieldOffsetX + x * sceneCellSize, 0, fieldOffsetX + x * sceneCellSize, sceneRows * sceneCellSize);
         }
-        return lines;
+
+        Rectangle rectangle = new Rectangle(0, 0, sceneWidth, sceneHeight);
+        rectangle.setFill(Color.TRANSPARENT);
+        rectangle.setStroke(Color.BLACK);
+
+        lines.getChildren().add(rectangle);
+    }
+
+    private void addGridLine(double startX, double startY, double endX, double endY) {
+        Line line = new Line(startX, startY, endX, endY);
+        line.setStroke(gridColor);
+        line.setStrokeWidth(0.5);
+        lines.getChildren().add(line);
     }
 
     private void redraw() {
@@ -199,17 +218,38 @@ public class Main extends Application {
         Set<Point> allAliveCells = space.getAllAliveCells();
         sceneCells.getChildren().clear();
         allAliveCells.stream()
-                .filter(p -> p.getX() >= sceneLeft && p.getX() <= sceneRight && p.getY() >= sceneBottom && p.getY() <= sceneTop)
+                .filter(p -> p.getX() >= Math.floor(sceneLeft) && p.getX() < sceneRight && p.getY() > sceneBottom && p.getY() <= Math.ceil(sceneTop))
                 .forEach(point -> placeCell(point.getX(), point.getY()));
     }
 
     private void placeCell(int x, int y) {
+
         Shape cell;
-        cell = new Rectangle(
-                -sceneLeft * sceneCellSize + x * sceneCellSize + 0.5,
-                (sceneBottom - 1) * sceneCellSize + (sceneRows * sceneCellSize) - y * sceneCellSize + 0.5,
-                sceneCellSize - 1, sceneCellSize - 1
-        );
+        double sceneX = fieldOffsetX + x * sceneCellSize + 0.5;
+        double sceneY = fieldOffsetY - y * sceneCellSize + 0.5;
+        double width = sceneCellSize - 1;
+        double height = sceneCellSize - 1;
+
+        if (sceneX < 0) {
+            width -= -sceneX;
+            sceneX = 0;
+        }
+
+        if (sceneY < 0) {
+            height -= -sceneY;
+            sceneY = 0;
+        }
+
+        if (sceneX + width > sceneWidth) {
+            width -= sceneX + width - sceneWidth;
+        }
+
+        if (sceneY + height > sceneHeight) {
+            height -= sceneY + height - sceneHeight;
+        }
+
+        cell = new Rectangle(sceneX, sceneY, width, height);
+
         cell.setFill(cellColor);
         sceneCells.getChildren().add(cell);
     }
